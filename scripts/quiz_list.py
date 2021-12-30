be2145c03b91d0e6179f9a18eda322cd0a564126
Avinash Varna <avinash.varna@gmail.com>
2021-12-29 20:01:28 -0700

    Added missing num_questions field

--- scripts/quiz_list.py
+++ scripts/quiz_list.py
@@ -21,8 +21,9 @@ if __name__ == "__main__":
     for file in files:
         with open(file, encoding='utf-8') as f:
             quiz = json.load(f)
-            quiz.pop('questions')
+            questions = quiz.pop('questions')
             quiz['file'] = file.as_posix()
+            quiz['num_questions'] = len(questions)
             quizzes.append(quiz)
 
     with open('build/quizzes.json', 'w', encoding='utf-8') as f:
